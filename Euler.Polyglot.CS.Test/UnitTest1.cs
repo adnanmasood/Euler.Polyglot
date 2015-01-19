41774c2a5a198ada56e5058856f2ffe583802bf7
Adnan Masood <adnanmasood@gmail.com>
2015-01-18 22:51:50 -0500

    Problem 2

--- Euler.Polyglot.CS.Test/UnitTest1.cs
+++ Euler.Polyglot.CS.Test/UnitTest1.cs
@@ -5,7 +5,7 @@ using Euler.Polyglot;
 namespace Euler.Polyglot.CS.Test
 {
     [TestClass]
-    public class TestProblem1
+    public class TestProblems
     {
         [TestMethod]
         public void TestMethod1()
@@ -14,5 +14,13 @@ namespace Euler.Polyglot.CS.Test
             var sum = p1.SumArray(p1.FindMultiples(10, new int[] { 3, 5 }));
             sum = p1.SumArray(p1.FindMultiples(1000, new int[] { 3, 5 })); 
         }
+
+        [TestMethod]
+        public void Problem2Test()
+        {
+            var p2 = new Problem2();
+            var sum = p2.FibonnaciSum(4000000);
+        }
+
     }
 }
---
+++ Euler.Polyglot.FS/Problem2.fs
@@ -0,0 +1,13 @@
+﻿namespace Euler.Polyglot.FS
+open System
+
+type Problem2() = 
+    //member this.Total = [1..999] |> List.map (fun i -> if i % 5 = 0 || i % 3 = 0 then i else 0) |> List.sum
+    let fibonacciSeq = Seq.unfold (fun (current, next) -> Some(current, (next, current + next))) (0, 1)
+    let fibTotal =
+        fibonacciSeq
+        |> Seq.takeWhile (fun n -> n < 4000000)
+        |> Seq.filter (fun n -> n % 2 = 0)
+        |> Seq.sum
+    
+//Problem2();;
