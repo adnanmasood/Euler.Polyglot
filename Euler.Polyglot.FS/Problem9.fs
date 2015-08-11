ea95f78fb333462b82fb2e94269b461868b816f9
Adnan Masood <adnanmasood@gmail.com>
2015-08-11 17:50:58 -0400

    Problem 9 added

---
+++ Euler.Polyglot.FS/Problem9.fs
@@ -0,0 +1,19 @@
+﻿module Problem9
+
+open System.IO
+
+let isPythagoreanTri(numbers : int list) =
+    match List.sort(numbers) with
+    | [a; b; c] -> a*a + b*b = c*c
+    | _ -> false
+ 
+let getTri =
+    seq {
+        for a = 1 to 1000 do
+            for b = 1 to 1000 do
+                for c = 1 to 1000 do
+                    if a + b + c = 1000 then yield [a; b; c]
+    }
+ 
+let pythagoreanTri = getTri |> Seq.filter isPythagoreanTri |> Seq.head
+let product = pythagoreanTri |> Seq.fold (fun acc x -> acc * x) 1
